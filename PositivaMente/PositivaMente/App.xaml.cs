5ef7cccf67255d0094740f64f15f5a84d18fd322
Isidro Escoda <isidro.escoda@gmail.com>
2019-12-18 12:38:25 +0100

    First Commit - App layout

---
+++ PositivaMente/PositivaMente/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PositivaMente.Services;
+using PositivaMente.Views;
+
+namespace PositivaMente
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PositivaMente/PositivaMente/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using PositivaMente.Models;
+
+namespace PositivaMente.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
